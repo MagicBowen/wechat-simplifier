c1473d83645cdf2045e4246675a820dba648d869
magicbowen <e.bowen.wang@icloud.com>
2018-05-10 14:01:47 +0800

    modify

--- run.py
+++ run.py
@@ -33,6 +33,10 @@ def get_reply(query):
     if not query or query.strip() == '':
         return '对不起，不能发送空消息哦~'
 
+    if query == 'x' and last_reply.strip() != '':
+        file.write(last_reply)
+        return '已标记，谢谢！'
+
     request_json = { "x": query } 
 
     headers = {'Content-Type': 'application/json'}
@@ -41,37 +45,25 @@ def get_reply(query):
         if response.status_code != 200: 
             logger.error('requested chatbot failed, error code = {0}!'.format(response.status_code))
             return '升级维护中，请稍后再试...'
-        return response.json()['y']
+        last_reply = response.json()['y']
+        return last_reply
     except Exception as e:
         logger.error('requested chatbot exception: {0}!'.format(str(e)))
         return '升级维护中，请稍后再试...'
-
-#########################################################
-def send_reply(query):
-    rsp = get_reply(content)
-    print("response text: " + rsp)
-    last_reply = rsp
-    return rsp    
     
 #########################################################
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):
     user_id = msg['FromUserName']
     content = msg['Content']
-    print("receive text: " + content)
-    rsp = get_reply(content)
-    print("response text: " + rsp)
-    return rsp
+    return get_reply(content)
 
 #########################################################
 @itchatmp.msg_register(itchatmp.content.VOICE)
 def voice_reply(msg):
     user_id = msg['FromUserName']
     content = msg['Recognition']
-    print("receive voice: " + content)
-    rsp = get_reply(content)
-    print("response text: " + rsp)
-    return rsp
+    return get_reply(content)
 
 #########################################################
 @itchatmp.msg_register(itchatmp.content.EVENT)
@@ -79,13 +71,13 @@ def subscribe_reply(msg):
     user_id = msg['FromUserName']
     if msg['Event'] == 'subscribe' :
         print("receive event: subscribe")
-        return "欢迎加入小哒智能AI内测训练！"
+        return "欢迎加入小哒智能AI内测训练，回复 x 对不正确的语义结果进行标记，以帮助随后更好的训练，谢谢！"   
     elif msg['Event'] == 'CLICK' :
         print("receive event: CLICK")
         key = msg['EventKey']
         if key == 'modify':
             file.write(last_reply)
-            return '已记录'    
+            return '已记录'
 
-register_menu()
-itchatmp.run(port=config.HTTP_PORT)    
+# register_menu()
+itchatmp.run(port=config.HTTP_PORT)
